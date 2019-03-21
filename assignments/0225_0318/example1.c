fc6345774ce3adc2af1eb75b197e7caddae12ac9
Kevin00 <K_code@163.com>
2019-03-21 16:48:26 +0800

    Create example1.c

---
+++ assignments/0225_0318/example1.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define ML 2333
+
+char buf[ML], *cur = buf;
+
+void ReadExpr(void)
+{
+	char *p, *q;
+
+	scanf("%[^\n]", buf);
+	for (p = q = buf; *p != '\0'; ++p)
+		if (*p != ' ' && *p != '=')
+			*q++ = *p;
+	*q = '\0';
+}
+
+int ScanNumber(void)
+{
+	int ret = 0, sign = 1;
+	if (*cur == '-')
+	{
+		++cur;
+		sign = -1;
+	}
+
+	while (isdigit(*cur))
+	{
+		ret *= 10;
+		ret += *cur - '0';
+		++cur;
+	}
+
+	return sign * ret;
+}
+
+int CountFactor(void)
+{
+	int ret = ScanNumber();
+	while (*cur == '*' || *cur == '/')
+	{
+		char op = *cur++;
+		int next = ScanNumber();
+		if (op == '*')
+			ret *= next;
+		else
+			ret /= next;
+	}
+	return ret;
+}
+
+int CountExpr(void)
+{
+	int ret = CountFactor();
+	while (*cur == '+' || *cur == '-')
+	{
+		char op = *cur++;
+		int next = CountFactor();
+		if (op == '+')
+			ret += next;
+		else
+			ret -= next;
+	}
+	return ret;
+}
+
+int main()
+{
+	ReadExpr();
+	printf("%d", CountExpr());
+
+	return 0;
+}
