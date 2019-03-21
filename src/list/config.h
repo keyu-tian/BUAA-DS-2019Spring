1714fda691c369363a0571aa3e52692f33c86e9a
Kevin00 <K_code@163.com>
2019-03-21 15:50:48 +0800

    Create config.h

---
+++ src/list/config.h
@@ -0,0 +1,18 @@
+#ifndef INC_K_LIST_CONFIG
+#define INC_K_LIST_CONFIG
+
+
+typedef enum {false, true} bool;
+
+typedef int Data;
+typedef int cint;	// integer for counting
+typedef int xint;	// integer for indexing
+
+
+extern const Data INVALID_DATA;
+extern const xint INVALID_INDEX;
+extern const xint ZERO_INDEX;
+extern const cint CAPACITY;
+
+
+#endif
